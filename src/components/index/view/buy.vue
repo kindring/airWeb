3613982b6d60b7368c1748aeae832b58a832574b
kindring <2460131004@qq.com>
2022-04-26 12:01:15 +0800

    订单创建页面完善

--- src/components/index/view/buy.vue
+++ src/components/index/view/buy.vue
@@ -13,7 +13,71 @@
             <a-step title="等待飞行" description="等待航班飞行"/>
             <a-step title="完成" description="航班结束,不可进行操作"/>
           </a-steps>
+          <div class="py-2 flex">
+            航班号:{{flightData.flightName}}
+          </div>
+          <div class="py-2 flex">
+            飞机代号:{{flightData.airCode}}
+          </div>
+          <div class="py-2 flex">
+            乘机人:{{nickName}} ,登机手机号:{{phone}}
+          </div>
+          <!--          出发时间地点-->
+          <a-timeline class="mt-2">
+            <!--              出发时间 -->
+            <a-timeline-item class=" flex flex-col justify-center">
+              <div class="px-1.5 w-2/4 relative top-1 rounded py-1 flex bg-yellow-300 overflow-hidden text-white">
+                <div class="text-black py-0.5 flex items-center text-5xl">
+                  <b><svg-icon class="mx-1" icon-class="fly"/></b>
+                </div>
+                <div class="text-black">
+                  <div class="py-0.5 text-xl">
+                    <b>{{flightData.departureCityName}}</b></div>
+                  <div class="py-0.5">{{moment((flightData.sailingTime-0)*1000).format('YY-MM-DD hh:mm:ss')}}</div>
+                </div>
 
+              </div>
+            </a-timeline-item>
+            <!--              到站 -->
+            <a-timeline-item class="py-5 flex flex-col justify-center">
+              <div class="px-2.5 w-2/4 relative top-1 rounded py-1 flex bg-green-300 overflow-hidden text-white">
+                <div class="text-black py-0.5 flex items-center text-5xl">
+                  <b><svg-icon class="mx-1" icon-class="dz"/></b>
+                </div>
+                <div class="div px-3 flex text-3xl items-center">
+                  <b>---</b>
+                </div>
+                <div class="text-black">
+                  <div class="py-0.5 text-xl">
+                    <b>{{flightData.targetCityName}}</b></div>
+                  <div class="py-0.5">{{moment((flightData.langdinTime-0)*1000).format('YY-MM-DD hh:mm:ss')}}</div>
+                </div>
+
+              </div>
+            </a-timeline-item>
+          </a-timeline>
+          <div class="flex py-2 mt-2 items-center">
+            航班状态:
+            <div v-if="flightData.flightState === '1'" class="px-2.5 flex items-center ml-3 bg-green-200 py-1" >
+              正在售票中
+            </div>
+            <div v-else-if="flightData.flightState === '2'" class="px-2.5 flex items-center ml-3 bg-blue-300 py-1" >
+              正在检票中
+            </div>
+            <div v-else-if="flightData.flightState === '3'" class="px-2.5 flex items-center ml-3 bg-yellow-200 py-1" >
+              飞行中
+            </div>
+            <div v-else class="px-2.5 flex items-center ml-3 bg-gray-400 py-1" >
+              航班已经结束
+            </div>
+          </div>
+
+          <div class="flex py-2.5 border-t">
+            <a-button-group>
+              <a-button>取消</a-button>
+              <a-button >提交订单</a-button>
+            </a-button-group>
+          </div>
         </div>
 <!--        加载失败展示框-->
         <template v-slot:loadFail>
@@ -34,6 +98,8 @@ import handle from "@/utils/handle";
 import api_flights from "@/apis/api_flight";
 import business from "@/utils/business";
 import moment from "moment";
+import {mapState} from "vuex";
+import types from "@/store/homeTypes";
 
 export default {
   name: "buy",
@@ -46,6 +112,15 @@ export default {
       langdinTime:null
     },
   }},
+  computed:{
+    ...mapState({
+      // 城市列表
+      isLogin:state=>state.user[types.user.state.isLogin],
+      nickName:state=>state.user[types.user.state.nickName],
+      phone:state=>state.user[types.user.state.phone],
+      // 用户选择的
+    }),
+  },
   mounted(){
     if(!this.$route.query.flightId){
       this.loadingState = 2;
@@ -56,6 +131,7 @@ export default {
     this.loadInfo();
   },
   methods:{
+    moment,
     async loadInfo(){
       if(!this.flightId){
         return this.$message.error('请输入航班id');
